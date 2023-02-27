f051c6e4244ce4aafb1a15b22fbe005b8bea1c4e
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-27 13:42:15 -0600

    post route for api not working trying new routes

--- Develop/server.js
+++ Develop/server.js
@@ -9,9 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/api', api);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // GET route for homepage
 app.get('/', (req, res) => {
@@ -21,10 +22,37 @@ app.get('/', (req, res) => {
 
 // GET route for notes page
 app.get('/notes', (req, res) => 
-    res.json(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 // wildcard route to direct users to homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+
+// Get route for api notes
+app.get('/api/notes', function(req, res){
+    // res.json(path.join(__dirname, '/db/db.json'));    
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+})
+
+app.get('/api/notes', function(req, res) {
+    res.json(path.join(__dirname, 'db/db.json'));
+});
+
+// Post route for api notes
+app.post('/api/notes', function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    let newNote = req.body;
+    let id = (savedNotes.length).toString();
+    newNote.id = id;
+    savedNotes.push(newNote);
+    // Write to json file
+    fs.writeFileSync('db/db.json', JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
+
+// listener
+app.listen(PORT, () => {
+    console.log(`App listening on localhost:${PORT}`);
+  });
