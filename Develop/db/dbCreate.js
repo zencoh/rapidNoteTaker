955a80645c2978a9cba4d8b39e65ef76c11df90f
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-27 21:47:51 -0600

    all routes work properly

--- Develop/db/dbCreate.js
+++ Develop/db/dbCreate.js
@@ -1,19 +1,18 @@
 // define require for fs and util
-const fs = require( 'fs' );
-const util = require( 'util' );
+const fs = require('fs');
+const util = require('util');
 
 // UUID
-const uuid = require( '../helpers/uuid' );
-const readFileAsync = util.promisify( fs.readFile );
-const writeFileAsync = util.promisify( fs.writeFile );
+const uuid = require('../helpers/uuid');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class dbCreate {
     read() {
-        return readFileAsync( 'db/db.json', 'utf8' );
+        return readFileAsync('db/db.json', 'utf8');
     }
     getNotes() {
         return this.read().then((data) => {
-
             // create if / else / return statement for [...data] attribute
             data = JSON.parse(data);
             if(data.length === 0){
@@ -22,32 +21,32 @@ class dbCreate {
         }).then(notes => notes);
     }
     write(note) {
-        return writeFileAsync( 'db/db.json', JSON.stringify(note) );
+        return writeFileAsync('db/db.json', JSON.stringify(note));
     }
     addNote(newNote) {
-        let { title, text } = newNote;
+        let {title, text} = newNote;
         const note = {
             title,
             text,
             id: uuid()
         };
-        return this.getNotes().then( (allNotes) => {
+        return this.getNotes().then((allNotes) => {
             return [...allNotes, note];
-        }).then( ( updateNotes ) => {
+        }).then((updateNotes) => {
             return this.write( updateNotes );
-        }).then( () => note);
+        }).then(() => note);
     }
     deleteNote(id) {
-        return this.getNotes().then( ( allNotes ) =>         {
+        return this.getNotes().then((allNotes) => {
             let newNotes = [];
             for(let i = 0; i < allNotes.length; i++)
             {
                 if(allNotes[i].id != id ){
-                    newNotes.push( allNotes[i] );
+                    newNotes.push(allNotes[i]);
                 };
             } return newNotes;
-        }).then( ( byeNotes ) => {
-            return this.write( byeNotes )
+        }).then((endNotes) => {
+            return this.write(endNotes)
         });
     };
 };
--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,32 +1,30 @@
 // import files
-let fs = require( 'fs' );
-let util = require( 'util' );
-let store = require( '../db/dbCreate.js' );
+let fs = require('fs');
+let util = require('util');
+let store = require('../db/dbCreate.js');
 
-// set paths (from store.js)
-// let readFileAsync = util.promisify( fs.readFile );
-// let writeFileAsync = util.promisify( fs.writeFile );
+// set paths (from dbCreate.js)
+// let readFileAsync = util.promisify(fs.readFile);
+// let writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app) {
-
 // GET
-app.get( '/api/notes', function(req, res) {
+app.get('/api/notes', function(req, res) {
     store.getNotes().then((data) => {
         return res.json(data);
     });
 });
-
 // POST
-app.post( '/api/notes', function(req, res) {
+app.post('/api/notes', function(req, res) {
     store.addNote(req.body).then((note) => {
         return res.json(note);
     });
 });
 
 // DELETE
-// app.delete( '/api/notes/:id', function(req, res){
-//     store.deleteNote(req.params.id).then(() => {
-//         return res.json( {ok:true} );
-//     });
-// });
+app.delete('/api/notes/:id', function(req, res){
+    store.deleteNote(req.params.id).then(() => {
+        return res.json({ok:true});
+    });
+});
 }
